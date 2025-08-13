eb1cf21857d4a6930b1db69129c82c18ddec2761
dsfsgs <denieltomenio04@gmail.com>
2025-08-13 13:12:09 +0800

    fixing sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,7 +6,7 @@
         <q-btn flat no-caps no-wrap class="q-ml-xs">
           <q-icon name="local_pharmacy" color="red" size="28px" />
           <q-toolbar-title shrink class="text-weight-bold">
-            City Health Office - Inventory System
+            Medical Assistance for Indigent and Financial Incapacitated Patients System
           </q-toolbar-title>
         </q-btn>
 
@@ -21,10 +21,10 @@
             <q-tooltip>Account</q-tooltip>
 
             <q-list style="min-width: 150px">
-              <q-item clickable v-close-popup @click=" $router.push('/users/user/')">
+              <q-item clickable v-close-popup @click="$router.push('/users/user/')">
                 <q-item-section>
                   <q-item-label>
-                    <q-icon name="account_circle"  size="20px"></q-icon>
+                    <q-icon name="account_circle" size="20px"></q-icon>
                     Account
                   </q-item-label>
                 </q-item-section>
@@ -32,7 +32,7 @@
               <q-item clickable v-close-popup @click="logout()">
                 <q-item-section>
                   <q-item-label>
-                     <q-icon name="logout" size="20px"></q-icon>
+                    <q-icon name="logout" size="20px"></q-icon>
                     Logout
                   </q-item-label>
                 </q-item-section>
@@ -43,124 +43,57 @@
       </q-toolbar>
     </q-header>
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-green-10 text-white">
-      <q-scroll-area class="fit">
-        <q-list>
-          <q-item> </q-item>
-
-          <q-item clickable v-ripple to="/dashboard">
+      <q-list>
+        <q-item></q-item>
+        <q-item clickable v-ripple to="/dashboard">
+          <div class="row items-center">
             <q-icon name="dashboard" size="24px" class="q-mr-md" />
-            <p class="text-sm">Dashboard</p>
-          </q-item>
-          <q-separator />
-          <q-expansion-item
-            v-model="expanded"
-            label="Client Management"
-            icon="app_registration"
-            class="text-subtitle2"
-          >
-            <q-item clickable v-ripple to="/customer/orders">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="pallet" class="q-ml-md q-mr-md" size="24px" />
-                  Releasing
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-              <q-item clickable v-ripple to="/ris/orders">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="receipt" class="q-ml-md q-mr-md" size="24px" />
-                  Requisition and Issuance
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple to="/customers">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="person" class="q-ml-md q-mr-md" size="24px" />
-                  Customer History
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-expansion-item>
-          <q-expansion-item
-            label="Stocks Management"
-            expand-separator
-            icon="inventory"
-            class="text-subtitle2"
-            v-model="expanded"
-          >
-            <q-item clickable v-ripple to="/items/list">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="get_app" class="q-ml-md q-mr-md" size="24px" />Stock
-                  Purchases</q-item-label
-                >
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple to="/inventory/adjustment">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="lock_clock" class="q-ml-md q-mr-md" size="24px" />Stock
-                  Adjustment</q-item-label
-                >
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple to="/items/movement">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="timeline" class="q-ml-md q-mr-md" size="24px" />Stock
-                  Inventory</q-item-label
-                >
-              </q-item-section>
-            </q-item>
-          </q-expansion-item>
+            <span class="text-sm">Dashboard</span>
+          </div>
+        </q-item>
 
-          <q-expansion-item
-            label="Reports"
-            expand-separator
-            icon="bar_chart"
-            class="text-subtitle2"
-            v-model="expanded"
-          >
-            <q-item clickable v-ripple to="/items/reports">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon
-                    name="production_quantity_limits"
-                    class="q-ml-md q-mr-md"
-                    size="24px"
-                  />Stocks
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-expansion-item>
+        <q-item clickable v-ripple to="/customers">
+          <div class="row items-center">
+            <q-icon name="person" size="24px" class="q-mr-md" />
+            <span class="text-sm">Patient Info</span>
+          </div>
+        </q-item>
 
-          <q-expansion-item
-            label="Settings"
-            expand-separator
-            icon="settings"
-            class="text-subtitle2"
-            v-model="expanded"
-          >
-            <q-item clickable v-ripple to="/users/list">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="manage_accounts" class="q-ml-md q-mr-md" size="24px" />Users
-                  Management
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-ripple to="/libraries/list">
-              <q-item-section class="q-ml-sm">
-                <q-item-label class="text-caption">
-                  <q-icon name="category" class="q-ml-md q-mr-md" size="24px" />Libraries
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-expansion-item>
-        </q-list>
-      </q-scroll-area>
+        <q-item clickable v-ripple to="">
+          <div class="row items-center">
+            <q-icon name="assignment" size="24px" class="q-mr-md" />
+            <span class="text-sm">Assessment</span>
+          </div>
+        </q-item>
+
+        <q-item clickable v-ripple to="">
+          <div class="row items-center">
+            <q-icon name="forum" size="24px" class="q-mr-md" />
+            <span class="text-sm">Consultation</span>
+          </div>
+        </q-item>
+
+        <q-item clickable v-ripple to="">
+          <div class="row items-center">
+            <q-icon name="science" size="24px" class="q-mr-md" />
+            <span class="text-sm">Laboratory</span>
+          </div>
+        </q-item>
+
+        <q-item clickable v-ripple to="">
+          <div class="row items-center">
+            <q-icon name="receipt_long" size="24px" class="q-mr-md" />
+            <span class="text-sm">Billing</span>
+          </div>
+        </q-item>
+
+        <q-item clickable v-ripple to="/users/list">
+          <div class="row items-center">
+            <q-icon name="supervisor_account" size="24px" class="q-mr-md" />
+            <span class="text-sm">User Management</span>
+          </div>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -171,7 +104,7 @@
 
 <script>
 import auth from 'src/services/auth'
-import {useUserStore} from 'src/stores/userStore'
+import { useUserStore } from 'src/stores/userStore'
 export default {
   name: 'MyLayout',
   components: {},
@@ -181,7 +114,7 @@ export default {
     const userStore = useUserStore()
     return {
       ausSrvc,
-      userStore
+      userStore,
     }
   },
   data() {
@@ -195,22 +128,22 @@ export default {
       this.leftDrawerOpen = !this.leftDrawerOpen
     },
 
-    async logout(){
+    async logout() {
       try {
         await this.ausSrvc.logout()
         this.$router.push('/')
       } catch (error) {
-        console.log (error)
+        console.log(error)
       }
     },
 
-    GetUserID(){
+    GetUserID() {
       const unsanitized_object = localStorage.getItem('user')
       const sanitized_object = unsanitized_object.replace('__q_objt|', '')
       const user = JSON.parse(sanitized_object)
       this.userStore.authenticatedUser = user.id
-     // console.log(user.id)
-    }
+      // console.log(user.id)
+    },
   },
 
   watch: {},
