b3a1f59cca92ebc9e505545a2e11225ac24eba55
dsfsgs <denieltomenio04@gmail.com>
2025-11-11 12:29:37 +0800

    Fix Billing Gl Modal Button

--- src/router/routes.js
+++ src/router/routes.js
@@ -111,7 +111,7 @@ const routes = [
       },
       {
         path: '/philhealth/report',
-        component: () => import('pages/AssistancePhilHealthPage.vue'),
+        component: () => import('pages/AssistancePhilhealthPage.vue'),
         meta: { requiresAuth: true },
       },
 
