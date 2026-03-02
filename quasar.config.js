e63975db7e3ee78f5b33d7e692995d651dea25a2
jogz44 <annjogz@gmail.com>
2026-03-02 09:27:14 +0800

    push before merge

--- quasar.config.js
+++ quasar.config.js
@@ -48,7 +48,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/champ',
+      publicPath: '/chopa',
       // analyze: true,
       // env: {},
       // rawDefine: {}
